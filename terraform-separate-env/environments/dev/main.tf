744da778c0fe54ef4361b8279daf1263ab6fbe9b
mochi-yu <mochi.2225.it@gmail.com>
2024-07-15 00:23:07 +0900

    fix: 共通部分の移動

--- terraform-separate-env/environments/dev/main.tf
+++ terraform-separate-env/environments/dev/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">=1.7"
-
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-}
-
 module "app" {
   source = "../../modules"
   env = "develop"
