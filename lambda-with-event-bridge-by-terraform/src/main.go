59bc2ef32ecc773d542fe55e30fbc83d7ccf4021
mochi-yu <mochi.2225.it@gmail.com>
2024-07-14 19:52:24 +0900

    copy: プロジェクトをコピー

---
+++ lambda-with-event-bridge-by-terraform/src/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+type MyResponse struct {
+	Message string `json:"msg"`
+	Time    string `json:"time"`
+}
+
+func HandleRequest(ctx context.Context, event *MyEvent) (*MyResponse, error) {
+	if event == nil {
+		return nil, fmt.Errorf("received nil event")
+	}
+
+	msg := "Hello, " + event.Name + "! This is Docker Lambda."
+	res := MyResponse{
+		Message: msg,
+		Time:    time.Now().String(),
+	}
+	log.Print(msg)
+
+	return &res, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
