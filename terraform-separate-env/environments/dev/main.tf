5799137d39143279ecdf8eee86ebf4685e9a5b61
mochi-yu <mochi.2225.it@gmail.com>
2024-07-15 19:18:18 +0900

    fix: S3で`tfstate`を管理する設定を追加

--- terraform-separate-env/environments/dev/main.tf
+++ terraform-separate-env/environments/dev/main.tf
@@ -2,3 +2,12 @@ module "app" {
   source = "../../modules"
   env = "develop"
 }
+
+terraform {
+  backend "s3" {
+    region  = "ap-northeast-1"
+    encrypt = false
+    # bucket  = "XXXXXXXXXXXXXXX"
+    # key     = "XXXXXXXXXXXXXXX"
+  }
+}
