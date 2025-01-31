f3781387d509f8ae49bda0c9c42d60d8d2beee36
mochi-yu <mochi.2225.it@gmail.com>
2025-02-01 01:20:46 +0900

    feat: Go言語でのTLSのC/Sを実装

---
+++ golang-tls-communication/client/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+)
+
+func main() {
+	conf := &tls.Config{
+		InsecureSkipVerify: true, // 検証のためにTLSの検証をスキップ
+	}
+
+	conn, err := tls.Dial("tcp", "127.0.0.1:12345", conf)
+	if err != nil {
+		log.Fatal("dial: ", err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("Hello, golang TLS!!\n"))
+	if err != nil {
+		log.Fatal("write: ", err)
+	}
+
+	buf := make([]byte, 100)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal("read: ", err)
+	}
+
+	log.Printf("Accept Message: `%s`", buf[:n])
+}
---
+++ golang-tls-communication/server/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"log"
+	"net"
+)
+
+func main() {
+	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Fatal("loadkeys: ", err)
+	}
+
+	config := &tls.Config{Certificates: []tls.Certificate{cer}}
+	ln, err := tls.Listen("tcp", ":12345", config)
+	if err != nil {
+		log.Fatal("listen: ", err)
+	}
+	defer ln.Close()
+
+	log.Println("Start Server:")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal("accept: ", err)
+		}
+
+		log.Println("New client connection accepted")
+		go func(conn net.Conn) {
+			r := bufio.NewScanner(conn)
+			for r.Scan() {
+				msg := r.Text()
+				log.Printf("Accept Message: `%s`", msg)
+				_, err := conn.Write([]byte(msg))
+				if err != nil {
+					log.Print("write: ", err)
+				}
+			}
+			conn.Close()
+			log.Println("Client connection closed")
+		}(conn)
+	}
+}
